6c0e8cf9440adae22af46a0000f978073bf8a7a3
santveraaa <santveraaa@gmail.com>
2025-08-29 07:33:53 +0700

    Update build.gradle.kts

--- Dramaid/build.gradle.kts
+++ Dramaid/build.gradle.kts
@@ -1,5 +1,3 @@
-
-
 group = "com.example.dramaid"
 version = "1.0.0"
 
@@ -11,18 +9,32 @@ repositories {
 
 dependencies {
     implementation(fileTree("libs") { include("*.jar") })
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")  // Ganti dengan versi yang stabil
 }
 
 // SOLUSI: Tambahkan JVM toolchain
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(17)  // Tentukan JVM 17 untuk Kotlin
 }
 
 // Opsional: Konfigurasi tambahan
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "17"  // Tentukan JVM 17 untuk Kotlin
         freeCompilerArgs = listOf("-Xjsr305=strict")
+    }
+}
+
+android {
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17  // Ganti dengan JVM 17
+        targetCompatibility = JavaVersion.VERSION_17  // Ganti dengan JVM 17
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"  // Tentukan JVM 17 untuk Kotlin
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
     }
 }
